9111c91e924f8712e53307a892028915a822be10
Deep Datta Roy <deepdattaroy8888@gmail.com>
2014-05-22 13:18:13 -0400

    Basic Map Working

--- GPS Logger/GPS Logger/HistoryViewController.m
+++ GPS Logger/GPS Logger/HistoryViewController.m
@@ -6,9 +6,11 @@
 //  Copyright (c) 2014 Deep Datta Roy. All rights reserved.
 //
 
+#import <MapKit/MapKit.h>
 #import "HistoryViewController.h"
 
-@interface HistoryViewController ()
+@interface HistoryViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -27,6 +29,50 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    _mapView.delegate = self;
+
+    //Simulated annotations on the map
+    CLLocationCoordinate2D poi1Coord , poi2Coord , poi3Coord , poi4Coord;
+    //poi1 coordinates
+    poi1Coord.latitude = 37.78754;
+    poi1Coord.longitude = -122.40718;
+    //poi2 coordinates
+    poi2Coord.latitude = 37.78615;
+    poi2Coord.longitude = -122.41040;
+    //poi3 coordinates
+    poi3Coord.latitude = 37.78472;
+    poi3Coord.longitude = -122.40516;
+    //poi4 coordinates
+    poi4Coord.latitude = 37.78866;
+    poi4Coord.longitude = -122.40623;
+    
+    MKPointAnnotation *poi1 = [[MKPointAnnotation alloc] init];
+    MKPointAnnotation *poi2 = [[MKPointAnnotation alloc] init];
+    MKPointAnnotation *poi3 = [[MKPointAnnotation alloc] init];
+    MKPointAnnotation *poi4 = [[MKPointAnnotation alloc] init];
+    
+    
+    poi1.coordinate = poi1Coord;
+    poi2.coordinate = poi2Coord;
+    poi3.coordinate = poi3Coord;
+    poi4.coordinate = poi4Coord;
+    
+    poi1.title = @"McDonald's";
+    poi1.subtitle = @"Best burgers in town";
+    poi2.title = @"Apple store";
+    poi2.subtitle = @"Iphone on sales..";
+    poi3.title = @"Microsoft";
+    poi3.subtitle = @"Microsoft's headquarters";
+    poi4.title = @"Post office";
+    poi4.subtitle = @"You got mail!";
+    
+    [_mapView addAnnotation:poi1];
+    [_mapView addAnnotation:poi2];
+    [_mapView addAnnotation:poi3];
+    [_mapView addAnnotation:poi4];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
--- GPS Logger/GPS Logger/IntroViewController.m
+++ GPS Logger/GPS Logger/IntroViewController.m
@@ -8,76 +8,91 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 #import "IntroViewController.h"
 
 
-@interface IntroViewController () <CLLocationManagerDelegate>
-@property (weak, nonatomic) IBOutlet UILabel *Latitude;
-@property (weak, nonatomic) IBOutlet UILabel *Longitude;
-@property (weak, nonatomic) IBOutlet UILabel *Address;
-- (IBAction)Log:(id)sender;
+@interface IntroViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
+
+    //Interaction with interface
+    @property (weak, nonatomic) IBOutlet UILabel *latitudeTextBox;
+    @property (weak, nonatomic) IBOutlet UILabel *longitudeTextBox;
+    @property (weak, nonatomic) IBOutlet UILabel *addressTextBox;
+    - (IBAction)logButton:(id)sender;
+
+    //Location of storage
+    @property (strong, nonatomic) NSString *dataFilePath;
+
 
 @end
 
 @implementation IntroViewController{
-CLLocationManager *manager;
-CLGeocoder *geocoder;
-CLPlacemark *placemark;
+    CLLocationManager *manager;
+    CLGeocoder *geocoder;
+    CLPlacemark *placemark;
 }
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    //Initialize the location structures
     manager = [[CLLocationManager alloc] init];
     geocoder = [[CLGeocoder alloc] init];
-    
+
 
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)Log:(id)sender {
+- (IBAction)logButton:(id)sender {
     
+    //Start updating the location
     manager.delegate = self;
     manager.desiredAccuracy = kCLLocationAccuracyBest;
-    
     [manager startUpdatingLocation];
     
 }
 
 #pragma mark CLLocationManagerDelegate Methods
 
-- (void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
-    NSLog(@"Error %@", error);
-    
-}
-
 - (void) locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
-    
-    NSLog(@"Location %@", newLocation);
-    CLLocation *currentLocation = newLocation;
-    
-    if (currentLocation != NULL){
-        self.Latitude.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
-        self.Longitude.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+
+    //If there is actually a location, update the view
+    if (newLocation != NULL){
+        self.latitudeTextBox.text = [NSString stringWithFormat:@"%.4f", newLocation.coordinate.latitude];
+        self.longitudeTextBox.text = [NSString stringWithFormat:@"%.4f", newLocation.coordinate.longitude];
     }
     
-    [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error) {
-        if (error == nil && [placemarks count] > 0){
+    //Try to reverse the coordinates to get address
+    [geocoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+        if (error == NULL && [placemarks count] > 0){
             placemark = [placemarks lastObject];
             
-            self.Address.text = [NSString stringWithFormat:@"%@ %@\n", placemark.locality, placemark.postalCode];
+            self.addressTextBox.text = [NSString stringWithFormat:@"%@\n%@ %@\n",placemark.thoroughfare, placemark.locality, placemark.postalCode];
             
-        }else{
-            NSLog(@"Error %@", error);
         }
     }];
     
-}
+    NSFileManager *filemgr;
+    NSString *docsDir;
+    NSArray *dirPaths;
+    filemgr = [NSFileManager defaultManager];
+    
+    // Get the documents directory
+    dirPaths = NSSearchPathForDirectoriesInDomains(
+                                                   NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    docsDir = dirPaths[0];
+    
+    // Build the path to the data file
+    _dataFilePath = [[NSString alloc] initWithString: [docsDir
+                                                       stringByAppendingPathComponent: @"data.archive"]];
 
+    
+}
 @end
